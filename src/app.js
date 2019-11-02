ab8027067a786e76380671573da6e77a23fe754f
Timothy Mehay <timothymehay@gmail.com>
2019-11-02 10:24:37 +0100

    Cleaned up app.js

--- src/app.js
+++ src/app.js
@@ -4,8 +4,6 @@ import { Provider } from 'react-redux';
 import AppRouter from './routers/AppRouter';
 import configureStore from './store/configureStore';
 
-import { addExpense } from './actions/expenses';
-import { setTextFilter } from './actions/filters';
 import getVisibleExpenses from './selectors/expenses';
 
 import 'normalize.css/normalize.css';
@@ -14,14 +12,9 @@ import './styles/styles.scss';
 
 const store = configureStore();
 
-store.dispatch(addExpense({ description: 'Water Bill', amount: 1000 }));
-store.dispatch(addExpense({ description: 'Gas Bill', amount: 1500, createdAt: 1000 }));
-store.dispatch(addExpense({ description: 'Rent', amount: 20500 }));
-
 store.subscribe(() => {
   const state = store.getState();
   const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-  console.log(visibleExpenses);
 });
 
 const App = (
