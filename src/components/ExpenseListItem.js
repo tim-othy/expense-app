4b51fff5fd9484192143f3ae61a662c9ba36f880
Timothy Mehay <timothymehay@gmail.com>
2020-01-04 15:40:27 +0100

    Styled expense list

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -6,17 +6,12 @@ import numeral from 'numeral';
 export default (props) => {
   const { description, amount, createdAt, id } = props;
   return (
-    <div>
-      <h3>
-        <Link to={`/edit/${id}`}>
-          {description}
-        </Link>
-      </h3>
-      <p>
-        {numeral(amount / 100).format('$0,0.00')}
-        -
-        {moment(createdAt).format('Do MMMM, YYYY')}
-      </p>
-    </div>
+    <Link className="list-item" to={`/edit/${id}`}>
+      <div>
+        <h3 className="list-item__title">{description}</h3>
+        <span className="list-item__sub-title">{moment(createdAt).format('Do MMMM, YYYY')}</span>
+      </div>
+      <h3 className="list-item__data">{numeral(amount / 100).format('$0,0.00')}</h3>
+    </Link>
   );
 }
